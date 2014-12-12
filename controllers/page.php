8c6c3fda07a75cd01ac18787e0ae1342be21f6e1
Ben Dixon <lumezious@googlemail.com>
2014-12-12 12:33:20 +0000

    Possible page fix

--- controllers/page.php
+++ controllers/page.php
@@ -2,9 +2,15 @@
 
 class Page extends Controller {
 
-	function display($f3) {
+	public function display($f3) {
 		$pagename = urldecode($f3->get('PARAMS.3'));
 		$page = $this->Model->Pages->fetch($pagename);
+
+		// If no page with the name exists, trigger a 404
+		if (empty($page)) {
+			return $f3->error(404);
+		}
+
 		$pagetitle = ucfirst(str_replace("_"," ",str_replace(".html","",$pagename)));
 		$f3->set('pagetitle',$pagetitle);
 		$f3->set('page',$page);
